f6f40eb8d178ed0213242a64927ead0e973eb761
ldynia <l_dynowski@yahoo.com>
2023-03-21 15:23:27 +0000

    adding libraries

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Install
 
-```bash
-rustc src/main.rs && ./main assets/img/rainbow.ipg
+## Cargo
 
-cargo install image
+```bash
+cargo add time
+cargo add image
+
+cargo install
+
+cargo build
+./target/debug/run assets/img/rainbow.jpg
+
+cargo build --release
+./target/release/run assets/img/rainbow.jpg
+
+cargo run assets/img/rainbow.jpg
+```
+
+## Compilation
+
+```bash
+rustc src/main.rs
+./main assets/img/rainbow.ipg
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+extern crate time;
+extern crate image;
+
+// use image::io::Reader as ImageReader;
 use std::env;
+// use std::io::Cursor;
 use std::path::PathBuf;
+
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -8,6 +14,12 @@ fn main() {
     let file = PathBuf::from(file);
     let file_path = file.canonicalize();
     println!("File Path: {}", file_path.unwrap().display());
+
+    let now = time::OffsetDateTime::now_utc();
+    println!("Time {now}");
+
+    // let res = ImageReader::into_dimensions("/workspaces/rust-image-filters/assets/img/rainbow.jpg");
+    // println!("{} {}", res.width);
   } else {
     println!("Error: Missing input file.");
   }
